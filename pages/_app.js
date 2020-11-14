bb55067164f5aa4b4feb69a11976dca7864ae823
Daniel Silva <danjsillva@gmail.com>
2020-11-14 09:49:04 -0300

    feat: adicionado script do google ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,6 +8,14 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
+      <Head>
+        <script
+          data-ad-client="ca-pub-8531805358090283"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+      </Head>
+
       <Component {...pageProps} />
 
       <ToastContainer
